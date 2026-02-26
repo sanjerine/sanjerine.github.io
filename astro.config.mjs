07a3de36b3aa4a1d3c56ca6788b180b1b927b978
sanjeevan <3gsanjee@gmail.com>
2026-02-26 08:19:10 +0800

    changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,11 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   site: "https://sanjerine.github.io",
   integrations: [mdx()],
+  markdown: {
+    shikiConfig: {
+      theme: "night-owl",
+    },
+  },
   vite: {
     plugins: [tailwindcss()],
   },
